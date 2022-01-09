80832dc5f1b75ca54ee2766b1d5507bb93919c3b
Reinaldo Ferreira Gez <reinaldogez@inf.ufsm.br>
2022-01-09 13:23:18 -0300

    adicionado readme, producer funcional

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net;
+using Confluent.Kafka;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace POC_Kafka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Produce("Test");
+        }
+
+        static void Produce(string topic)
+        {
+            var config = new ProducerConfig
+            {
+                //BootstrapServers = "host1:9092,host2:9092",
+                BootstrapServers = "localhost:29092",
+                
+                ClientId = Dns.GetHostName(),
+                //...
+            };
+
+
+            using (var producer = new ProducerBuilder<string, string>(config).Build())
+            {
+             int numProduced = 0;
+                int numMessages = 10;
+                for (int i=0; i<numMessages; ++i)
+                {
+                    var key = "alice";
+                    var val = JObject.FromObject(new { count = i }).ToString(Formatting.None);
+
+                    Console.WriteLine($"Producing record: {key} {val}");
+
+                    producer.Produce(topic, new Message<string, string> { Key = key, Value = val },
+                        (deliveryReport) =>
+                        {
+                            if (deliveryReport.Error.Code != ErrorCode.NoError)
+                            {
+                                Console.WriteLine($"Failed to deliver message: {deliveryReport.Error.Reason}");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Produced message to: {deliveryReport.TopicPartitionOffset}");
+                                numProduced += 1;
+                            }
+                        });
+                }
+
+                producer.Flush(TimeSpan.FromSeconds(10));
+
+                Console.WriteLine($"{numProduced} messages were produced to topic {topic}");
+            }
+        }
+    }
+}
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # POC-Kafka
 POC sobre o Apache Kafka como serviço de mensageria.
+=========
+
+## Comandos
+Criar um tópico
+~~~ps1
+docker exec kafka kafka-topics --create --bootstrap-server localhost:29092 --partitions 1 --replication-factor 1 --topic Test
+~~~
+
+## Referências
